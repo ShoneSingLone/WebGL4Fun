48f6b5cfadd5d73df87ded7c5d8604df247d455f
ShoneSingLone <ShoneSingLone@gmail.com>
2023-03-30 01:28:44 +0800

    style init

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ export const App = defineComponent({
 
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
+    document.getElementById("app").appendChild(renderer.domElement);
 
     const geometry = new THREE.BoxGeometry(1, 1, 1);
     const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
